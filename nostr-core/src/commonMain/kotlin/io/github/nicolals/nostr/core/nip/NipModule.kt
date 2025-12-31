248f4941d3442e7aba2248c13e48bd3fddf1ec66
NicolaLS <sus.nym@proton.me>
2025-12-31 13:24:01 +0100

    core: add nip module context
    The nip module context will provide json codec for content / payload
    parsing as well as other stuff that might need to be added later (for
    example crypto). This has the benefit that we avoid each nip module
    creating their own json or crypto instance. I do not like the ergonomics
    of having to pass this context to `MyEventView.template(ctx)` for
    example...but cant think of a better solution.

--- nostr-core/src/commonMain/kotlin/io/github/nicolals/nostr/core/nip/NipModule.kt
+++ nostr-core/src/commonMain/kotlin/io/github/nicolals/nostr/core/nip/NipModule.kt
@@ -1,8 +1,13 @@
 package io.github.nicolals.nostr.core.nip
 
+import io.github.nicolals.nostr.core.codec.JsonCodec
 import io.github.nicolals.nostr.core.event.upgrade.EventUpgradeRegistry
 import io.github.nicolals.nostr.core.event.upgrade.EventUpgrader
 import io.github.nicolals.nostr.core.event.view.EventView
+
+interface NipModuleContext {
+    val jsonCodec: JsonCodec
+}
 
 /**
  * Bundle event and template upgraders for a NIP so installation is explicit and consistent.
